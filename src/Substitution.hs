24d88baecc7190cdf2d23d2e342c3a5dc547f3ab
lakiryt <lakiryt@github.com>
2019-06-14 19:41:20 +0200

    Substitution: Added structural comments.

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -25,12 +25,17 @@ instance Show SingleSubst where
     show Subst{tup=(v1,v2)}  = show v1++"→"++(show v2) 
 
 
+-- Constructor
 infixl →
 (→) :: Var -> Var -> SingleSubst
 (→) bef aft = if isMeta bef
     then Subst (\v -> if v == bef then aft else v) (bef,aft)
     else error "substitution origin is non-meta"
 
+
+------------------------------------------------
+-- Substitution Application
+------------------------------------------------
 
 onVar :: Substitution -> (Var -> Var)
 onVar σ = foldl (.) id (map fun σ)
@@ -41,6 +46,10 @@ onBind σ (v1:=v2) = σ `onVar` v1 := (σ `onVar` v2)
 onExpr :: Substitution -> (Expr -> Expr)
 onExpr σ = map (σ `onBind`)
 
+
+------------------------------------------------
+-- Generalization
+------------------------------------------------
 
 data Token = B Bind | V Var | E Expr deriving (Eq,Ord,Show)
 
