b49e7669a7ff51ee15df873150f08c7fda3b2228
lakiryt <lakiryt@github.com>
2019-06-16 16:54:20 +0200

    Created Rules.hs; moved rule data type from Algorithm to Rules.

---
+++ src/Rules.hs
@@ -0,0 +1,36 @@
+module Rules
+    ( decomposition
+    ) where
+
+import Unification
+    ( SolverDS
+    , Equation((:=?:))
+    )
+import Substitution
+    ( Substitution
+    , Token(B,V)
+    )
+import Expression
+    ( Bind((:=))
+    )
+
+import qualified Data.Set as S
+
+------------------------------------------------
+-- Data Types
+------------------------------------------------
+
+type Input  = (Substitution, Equation, SolverDS)
+type Output = (Substitution, SolverDS)
+type Rule = Input -> [Output]
+
+
+
+infixr %
+(%) :: Equation -> SolverDS -> SolverDS
+(%) = S.insert
+
+
+decomposition :: Rule
+decomposition (sol,B (x:=y) :=?: B (x':=y'),γ) =
+            [(sol, (V x :=?: V x') % (V y :=?: V y') % γ)]
