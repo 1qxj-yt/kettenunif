e1a4af1804688856f3a98a64238926be17fb9961
lakiryt <lakiryt@github.com>
2019-06-18 15:23:48 +0200

    Algorithm: Implemented 'applyRuleFor'.

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -24,3 +24,14 @@ import Rules
     , application
     , orientation
     )
+applyRuleFor :: Equation -> Rule
+applyRuleFor (B _ :=?: B _)   = decomposition
+applyRuleFor (E [] :=?: E []) = tautology
+applyRuleFor (E e1 :=?: E e2) = if length e1 == length e2 then distribution else clash
+applyRuleFor (V v1 :=?: V v2)
+    | v1 == v2  = tautology
+    | otherwise = case (isMeta v1, isMeta v2) of
+        (False, False) -> if v1 == v2 then tautology else clash
+        (True , False) -> application
+        (False, True ) -> orientation
+        (True , True ) -> if v1 == v2 then tautology else application
