62d64a61219f8f855e7ddd53a69954d42395c0c5
lakiryt <lakiryt@github.com>
2019-06-18 15:13:57 +0200

    Algorithm: imported all rules, isMeta, and other types/functions from Substitution and UnifProblem.

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -1,2 +1,26 @@
 module Algorithm where
 
+import Expression
+    ( isMeta
+    )
+import Substitution
+    ( Substitution
+    , Token(E,B,V)
+    , identity
+    , compose
+    )
+import UnifProblem
+    ( UnifProblem
+    , SolverDS
+    , Equation((:=?:))
+    , probToSolver
+    )
+import Rules
+    ( Rule
+    , tautology
+    , clash
+    , distribution
+    , decomposition
+    , application
+    , orientation
+    )
