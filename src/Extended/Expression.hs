9646f5d0dabd9d5ef31ece1b72fb3b3ce5abe783
lakiryt <lakiryt@github.com>
2019-06-24 18:39:08 +0200

    Created Extended module (as a copy of Simple).

---
+++ src/Extended/Expression.hs
@@ -0,0 +1,52 @@
+module Extended.Expression
+    ( Expr
+    , Bind((:=))
+    , Var
+    , Token(B,V,E)
+    , meta
+    , var
+    , isMeta
+    ) where
+
+
+import Data.Char(isUpper,isLower)
+
+------------------------------------------------
+-- Data
+------------------------------------------------
+
+type Expr = [Bind]
+
+data Bind = Var := Var deriving (Eq,Ord) -- Ordering needed for S.Set
+
+data Var  = Var Char Integer | Meta Char Integer deriving (Eq,Ord)
+
+data Token = B Bind | V Var | E Expr deriving (Eq,Ord,Show)
+
+
+------------------------------------------------
+-- Constructors
+------------------------------------------------
+
+meta :: Char -> Integer -> Var
+meta c i  = if isUpper c then Meta c i else error "meta variable must be upper case"
+
+var  :: Char -> Integer -> Var
+var  c i  = if isLower c then Var  c i else error "variable must be lower case"
+
+
+isMeta :: Var -> Bool
+isMeta (Meta _ _) = True
+isMeta _         = False
+
+
+------------------------------------------------
+-- Show Instances
+------------------------------------------------
+
+instance Show Bind where
+    show (s := t) = show s++"="++show t
+
+instance Show Var where
+    show (Meta c i) = c:if i==0 then [] else show i
+    show (Var  c i) = c:if i==0 then [] else show i
---
+++ src/Extended/Rules.hs
@@ -0,0 +1,81 @@
+module Extended.Rules
+    ( Rule(name,apply)
+    , Input
+    , Output
+    , SSList(SSL)
+    -- * Rules
+    , tautology
+    , clash
+    , distribution
+    , decomposition
+    , application
+    , orientation
+    ) where
+
+import UnifProblem
+    ( SolverDS
+    , Equation((:=?:))
+    , onSolver
+    )
+import Substitution
+    ( Substitution
+    , (→)
+    )
+import Expression
+    ( Bind((:=))
+    , Token(E,B,V)
+    )
+
+import qualified Data.Set as S
+import Data.List(delete)
+
+------------------------------------------------
+-- Data Types
+------------------------------------------------
+-- | Just a list of substitutions,
+-- but meant to (type-)distinguish
+-- a list of sub-substitutions (especially single mappings)
+-- that will be composed to a whole solution later
+-- from a list of valid solution substitutions.
+--
+-- In the context of that motivation,
+-- @SSList@ is just a different representation of a whole @Substitution@.
+newtype SSList = SSL [Substitution]
+type Input  = (SSList, Equation, SolverDS)
+type Output = (SSList, SolverDS)
+data Rule   = R {name::String, apply::Input -> [Output]}
+
+instance Show Rule where
+    show = name
+
+instance Show SSList where
+    show (SSL list) = show list
+
+
+infixr %
+(%) :: Equation -> SolverDS -> SolverDS
+(%) = S.insert
+
+
+tautology :: Rule
+tautology = R "tautology" (\(sol,_,γ) -> [(sol,γ)])
+
+clash :: Rule
+clash = R "clash" (const [])
+
+distribution :: Rule
+distribution = R "distribution" (\(sol,E (b1:e1s) :=?: E e2,γ) ->
+            [(sol, (B b1 :=?: B b2) % (E e1s :=?: E (delete b2 e2)) % γ) | b2<-e2 ] )
+
+decomposition :: Rule
+decomposition = R "decomposition"
+            (\ (sol, B (x:=y) :=?: B (x':=y'), γ) ->
+            [(sol, (V x :=?: V x') % (V y :=?: V y') % γ)] )
+
+application :: Rule
+application = R "application" (\(SSL sol,V v1:=?: V v2,γ) ->
+            [(SSL ((v1 → v2):sol), (v1 → v2) `onSolver` γ)] )
+
+orientation :: Rule
+orientation = R "orientation" (\(sol, x:=?:y, γ) ->
+            [(sol, (y:=?:x) % γ)] )
