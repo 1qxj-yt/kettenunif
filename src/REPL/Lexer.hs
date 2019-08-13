f2494a7c8b2bf0eb21c57fc54e67396f9133196c
lakiryt <lakiryt@github.com>
2019-08-13 05:35:38 +0200

    Did REPL the right way

---
+++ src/REPL/Lexer.hs
@@ -0,0 +1,38 @@
+module REPL.Lexer
+    ( brackets
+    , commaSep
+    , natural
+    , lexeme
+    ) where
+
+import Text.Parsec
+
+import qualified Text.Parsec.Token as Tok
+import qualified Text.Parsec.Language as Lang
+
+import Data.Functor.Identity (Identity)
+
+
+lexer :: Tok.GenTokenParser String () Identity
+lexer = Tok.makeTokenParser style
+
+style :: Tok.GenLanguageDef String () Identity
+style = Lang.emptyDef
+  { Tok.commentStart    = "{-"
+  , Tok.commentEnd      = "-}"
+  , Tok.commentLine     = "--"
+  , Tok.nestedComments  = True
+  , Tok.identStart      = letter
+  , Tok.identLetter     = alphaNum <|> oneOf "_'"
+  , Tok.opStart         = Tok.opLetter style
+  , Tok.opLetter        = oneOf ":!#$%&*+./<=>?@\\^|-~"
+  , Tok.reservedOpNames = []
+  , Tok.reservedNames   = []
+  , Tok.caseSensitive   = True
+  }
+
+
+brackets = Tok.brackets lexer
+commaSep = Tok.commaSep lexer
+natural = Tok.natural lexer
+lexeme = Tok.lexeme lexer
