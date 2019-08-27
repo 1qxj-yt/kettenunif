0ec811a9db8af2683462b5728678793581361543
lakiryt <lakiryt@github.com>
2019-08-27 09:59:46 +0200

    Added biset rules to rule switch

--- src/Simple/Algorithm.hs
+++ src/Simple/Algorithm.hs
@@ -96,6 +96,13 @@ ruleFor :: Equation -> Rule
 ruleFor (B _ :=?: B _)   = decomposition
 ruleFor (E (Expr []) :=?: E (Expr [])) = tautology
 ruleFor (E (Expr e1) :=?: E (Expr e2)) = if length e1 == length e2 then distribution else clash
+ruleFor (E (SingleSVarExpr sv1 e1) :=?: E (SingleSVarExpr sv2 e2))
+                | sv1 == sv2
+                    && null e1
+                    && null e2  = biset_tautology
+                | null e1       = biset_application
+                | null e2       = orientation
+                | otherwise     = biset_distribution
 ruleFor (E (SingleSVarExpr sv []) :=?: E e) = set_application
 ruleFor (E (SingleSVarExpr sv e1) :=?: E e) = set_distribution
 ruleFor (V v1 :=?: V v2)
