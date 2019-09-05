b1973c27dae549cac21cb141013139af0e8568f1
lakiryt <lakiryt@github.com>
2019-09-05 17:23:53 +0200

    corrected all test cases to use constructor function instead of real constructor

--- test/Simple/SubstitutionSpec.hs
+++ test/Simple/SubstitutionSpec.hs
@@ -62,12 +62,12 @@ spec = do
             let s = build [v 'X' → v 'Y', v 'Y' → v 'X']
             compose s s `shouldBe` identity
         it "{M→[X=y] | X→a}.id === {M→[X=y] | X→a}" $ do
-            let s = build [SetVar 0 →→ Expr [v 'X' := v 'a'], v 'X' → v 'a']
+            let s = build [SetVar 0 →→ expr [v 'X' := v 'a'], v 'X' → v 'a']
             compose s identity `shouldBe` s
         it "{A->B}.{M1->[Y=b]|Y->A} === {M1->[Y=b]|A->B,Y->B}" $ do
             let a = build [v 'A' → v 'B']
-                b = build [SetVar 1 →→ Expr [v 'Y' := v 'b'], v 'Y' → v 'A']
-                s = build [SetVar 1 →→ Expr [v 'Y' := v 'b'], v 'A' → v 'B', v 'Y' → v 'B']
+                b = build [SetVar 1 →→ expr [v 'Y' := v 'b'], v 'Y' → v 'A']
+                s = build [SetVar 1 →→ expr [v 'Y' := v 'b'], v 'A' → v 'B', v 'Y' → v 'B']
             compose a b `shouldBe` s
 
     -- Equivalence
@@ -152,4 +152,4 @@ transposeCBA :: Substitution
 transposeCBA = build [v 'A' → v 'B', v 'C' → v 'A', v 'B' → v 'C']
 
 exprAB_BB_CB :: Token
-exprAB_BB_CB = E (Expr [v 'A' := v 'B', v 'B' := v 'B', v 'C' := v 'B'])
+exprAB_BB_CB = E (expr [v 'A' := v 'B', v 'B' := v 'B', v 'C' := v 'B'])
